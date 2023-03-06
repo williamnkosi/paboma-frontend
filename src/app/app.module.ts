f3cc0bc4fd0f511f42b45c9727672ba83ad49f8f
williamnkosi <william.dev.acc@gmail.com>
2023-03-06 11:12:04 -0500

    Created the signin and cart components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderToolbarComponent } from './components/header-toolbar/header-toolbar.component';
+import { HeaderToolbarComponent } from './components/header/header-toolbar/header-toolbar.component';
+import { DrawerComponent } from './components/header/drawer/drawer.component';
+import { SignInComponent } from './components/header/sign-in/sign-in.component';
+import { CartComponent } from './components/header/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderToolbarComponent
+    HeaderToolbarComponent,
+    DrawerComponent,
+    SignInComponent,
+    CartComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
