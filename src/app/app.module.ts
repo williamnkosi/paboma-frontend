83d132ba5cbdfa1d205bd2477e919769e1e6f885
williamnkosi <william.dev.acc@gmail.com>
2023-03-06 10:58:56 -0500

    Created the Header-toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderToolbarComponent } from './components/header-toolbar/header-toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderToolbarComponent
   ],
   imports: [
     BrowserModule,
